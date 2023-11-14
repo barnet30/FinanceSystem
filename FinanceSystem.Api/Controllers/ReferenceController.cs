b5122b764a8d157e24839d25b34255c8d119fd3f
Pavel Gavrilichev <barneet30@gmail.com>
2023-11-14 18:28:13 +0300

    add reference controller

---
+++ FinanceSystem.Api/Controllers/ReferenceController.cs
@@ -0,0 +1,27 @@
+﻿using FinanceSystem.Abstractions.Models.References;
+using FinanceSystem.Services.Interfaces.References;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceSystem.Controllers;
+
+[Route("api/references")]
+[AllowAnonymous]
+public class ReferenceController : BaseController
+{
+    private readonly IReferenceService _referenceService;
+
+    public ReferenceController(IReferenceService referenceService)
+    {
+        _referenceService = referenceService;
+    }
+    
+    /// <summary>
+    /// Get list of payment categories
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("paymentCategories")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<PaymentCategoryDto>))]
+    public Task<IActionResult> PaymentCategories() =>
+        GetResult(async () => await _referenceService.PaymentCategories());
+}
---
+++ FinanceSystem.Services/Services/ReferenceService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FinanceSystem.Abstractions.Models.References;
+using FinanceSystem.Abstractions.Models.Result;
+using FinanceSystem.Data.Interfaces.References;
+using FinanceSystem.Services.Interfaces.References;
+
+namespace FinanceSystem.Services.Services;
+
+public class ReferenceService : IReferenceService
+{
+    private readonly IMapper _mapper;
+    private readonly IPaymentCategoryRepository _paymentCategoryRepository;
+
+    public ReferenceService(IPaymentCategoryRepository paymentCategoryRepository, IMapper mapper)
+    {
+        _paymentCategoryRepository = paymentCategoryRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<List<PaymentCategoryDto>>> PaymentCategories()
+    {
+        var result = await _paymentCategoryRepository.GetAll();
+        return Result<List<PaymentCategoryDto>>.FromValue(_mapper.Map<List<PaymentCategoryDto>>(result));
+    }
+}
